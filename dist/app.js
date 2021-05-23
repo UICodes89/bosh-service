d117fd737e5ea3d2b450b789866033ca7dbac098
Manoj Shukla <mshukla@allsorter.com>
2021-05-23 16:57:36 +0100

    initial commit

---
+++ dist/app.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+class App {
+    constructor() {
+        this.express = express();
+        this.mountRoutes();
+    }
+    mountRoutes() {
+        const router = express.Router();
+        router.get('/', (req, res) => {
+            res.json({
+                message: 'Hello World!'
+            });
+        });
+        router.get('/hello-world', (req, res) => {
+            res.json({
+                message: 'Hello World!'
+            });
+        });
+        this.express.use('/', router);
+    }
+}
+exports.default = new App().express;
+//# sourceMappingURL=app.js.map
